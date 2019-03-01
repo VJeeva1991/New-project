031cd5975901b96918fe752f7a081436127cc6cf
VJeeva1991 <jeeva.confident@gmail.com>
2019-03-01 12:47:26 +0530

    jeeva rem

--- src/Testcase1.java
+++ src/Testcase1.java
@@ -6,10 +6,7 @@ public class Testcase1 {
 		System.out.println("from villupuram");
 		System.out.println("jeeva");
 		System.out.println("guna");
-		System.out.println("guna is our greens class mate");
-		System.out.println("he is coming from ambathur");
-		System.out.println("jeeva jeeva jeeva");
-		System.out.println("guna guna");
+
 		
 		
 	}
